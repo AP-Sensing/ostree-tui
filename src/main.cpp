c0aed370df6cbe5c9af565f2715b268a4b37474a
Timon Ensel <forgottosave.ensel@tum.de>
2024-03-01 16:22:23 +0100

    Add commit parsing (dirty)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <iostream>
+#include <sstream>
 #include <memory> // for shared_ptr, allocator, __shared_ptr_access
 #include <stdexcept> 
 #include <string>
@@ -11,14 +12,79 @@
 #include "ftxui/component/component_base.hpp"      // for ComponentBase
 #include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
 #include "ftxui/dom/elements.hpp"  // for Element, operator|, text, center, border
- 
+#include "ftxui/dom/table.hpp"
 
 #include "ftxui/dom/elements.hpp"
 #include "ftxui/screen/screen.hpp"
 #include "ftxui/screen/string.hpp"
-#include <ftxui/component/screen_interactive.hpp> // for ScreenInteractive
+#include "ftxui/component/screen_interactive.hpp" // for ScreenInteractive
 
 using namespace ftxui;
+
+struct Commit {
+    std::string hash;
+    std::string parent;
+    std::string contentChecksum;
+    std::string date;
+    std::string subject;
+};
+
+std::vector<Commit> parseCommits(std::string ostreeLogOutput){
+  	std::vector<Commit> commitList;
+  
+  	std::stringstream log(ostreeLogOutput);
+  	std::string word;
+
+	Commit cur = {"error", "couldn't read commit", "", "", ""};
+
+  	size_t mode = 0;
+	bool ready = false;
+  	while (log >> word) {
+    	if (word == "commit") {
+			if (ready) {
+				commitList.push_back(std::move(cur));
+			}
+			ready = true;
+			// create new commit
+			cur = {"", "", "", "", ""};
+			log >> word;
+			cur.hash = word;
+		} else if (word == "Parent:") {
+			log >> word;
+			cur.parent = word;
+		} else if (word == "ContentChecksum:") {
+			log >> word;
+			cur.contentChecksum = word;
+		} else if (word == "Date:") {
+			log >> word;
+			cur.date = word;
+		}
+  	}
+	commitList.push_back(std::move(cur));
+
+	return commitList;
+}
+
+auto commitRender(std::vector<Commit> commits) {
+
+	auto firstcom = commits.size() > 0 ? commits.at(0).hash : "error";
+
+	int selected = 0;
+  	auto menu = Container::Vertical(
+      	{
+    	},
+      	&selected
+	);
+
+	for (auto commit : commits) {
+    	auto commitentry = MenuEntry(commit.hash);
+    	menu->Add(commitentry);
+		auto seperator = Renderer([] { return separator(); });
+		menu->Add(seperator);
+  	}
+
+	return menu;
+}
 
 // https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
 std::string exec(const char* cmd) {
@@ -36,33 +102,37 @@ std::string exec(const char* cmd) {
 }
 
 auto ostreeLog() {
-  // get log TODO make generic & for all refs at once
-  std::string ostreeLogOutput = exec("ostree log --repo=testrepo foo");
+  	// get log TODO make generic & for all refs at once
+  	std::string ostreeLogOutput = exec("ostree log --repo=testrepo foo");
 
-  return Renderer([ostreeLogOutput] { return paragraph(ostreeLogOutput) | center; });
+  	// parse commits
+	auto commits = parseCommits(ostreeLogOutput);
+
+	return commitRender(commits);
+  	return Renderer([ostreeLogOutput] { return paragraph(ostreeLogOutput) | center; });
 }
 
 int main(void) {
-  auto screen = ScreenInteractive::Fullscreen();
- 
-  auto log = ostreeLog();
-  auto right = Renderer([] { return text("manager") | center; });
-  auto header = Renderer([] { return text("OSTree TUI") | center; });
-  //auto bottom = Renderer([] { return text("bottom") | center; });
- 
-  int right_size = 30;
-  int top_size = 1;
-  //int bottom_size = 1;
- 
-  auto container = log;
-  container = ResizableSplitRight(right, container, &right_size);
-  container = ResizableSplitTop(header, container, &top_size);
-  //container = ResizableSplitBottom(bottom, container, &bottom_size);
- 
-  auto renderer =
-      Renderer(container, [&] { return container->Render() | border; });
- 
-  screen.Loop(renderer);
+  	auto screen = ScreenInteractive::Fullscreen();
+	
+  	auto log = ostreeLog();
+  	auto right = Renderer([] { return text("manager") | center; });
+  	auto header = Renderer([] { return text("OSTree TUI") | center; });
+  	//auto bottom = Renderer([] { return text("bottom") | center; });
+	
+  	int right_size = 30;
+  	int top_size = 1;
+  	//int bottom_size = 1;
+	
+  	auto container = log;
+  	container = ResizableSplitRight(right, container, &right_size);
+  	container = ResizableSplitTop(header, container, &top_size);
+  	//container = ResizableSplitBottom(bottom, container, &bottom_size);
+	
+  	auto renderer =
+  	    Renderer(container, [&] { return container->Render() | border; });
+	
+  	screen.Loop(renderer);
 
-  return EXIT_SUCCESS;
+  	return EXIT_SUCCESS;
 }
