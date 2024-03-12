a50c35180436638f9d2437ce172dc70db3ef0b7b
Timon Ensel <forgottosave.ensel@tum.de>
2024-03-12 16:15:00 +0100

    Refactor footer

---
+++ src/core/footer.cpp
@@ -0,0 +1,21 @@
+#include "ftxui/component/captured_mouse.hpp"  // for ftxui
+#include "ftxui/component/component.hpp"  // for Renderer, ResizableSplitBottom, ResizableSplitLeft, ResizableSplitRight, ResizableSplitTop
+#include "ftxui/component/component_base.hpp"      // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
+#include "ftxui/dom/elements.hpp"  // for Element, operator|, text, center, border
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/screen/string.hpp"
+
+#include "footer.h"
+
+using namespace ftxui;
+
+Component footer::footerRender() {
+	return Renderer([] {
+		return hbox({
+			text(" OSTree TUI ") | bold,
+			separator(),
+			text("  || exit: q || rebase_mode: r || apply changes: s ||  "),
+		});
+	});
+}
---
+++ src/core/footer.h
@@ -0,0 +1,15 @@
+#include "ftxui/component/captured_mouse.hpp"  // for ftxui
+#include "ftxui/component/component.hpp"  // for Renderer, ResizableSplitBottom, ResizableSplitLeft, ResizableSplitRight, ResizableSplitTop
+#include "ftxui/component/component_base.hpp"      // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
+#include "ftxui/dom/elements.hpp"  // for Element, operator|, text, center, border
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/screen/string.hpp"
+
+using namespace ftxui;
+
+namespace footer{
+
+    Component footerRender();
+
+} // namespace footer
