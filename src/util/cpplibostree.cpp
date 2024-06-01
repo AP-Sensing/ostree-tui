662f379e89115d9e3a6edebfe1b4dfa9cc2a0877
Timon Ensel <forgottosave.ensel@tum.de>
2024-06-02 01:44:34 +0200

    remove unused exec

--- src/util/cpplibostree.cpp
+++ src/util/cpplibostree.cpp
@@ -287,20 +287,6 @@ namespace cpplibostree {
         return branches_str;
     }
 
-    std::string exec(const char* cmd) {
-            std::array<char, 128> buffer;
-            std::string result;
-            std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-            if (!pipe) {
-                throw std::runtime_error("popen() failed!");
-            }
-            while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
-                result += buffer.data();
-                result += " ";
-            }
-            return result;
-        }
-
     /// TODO This implementation is dirty, it should not rely on the ostree CLI
     bool OSTreeRepo::promoteCommit(const std::string& hash, const std::string& newRef, const std::string& newSubject, bool keepMetadata) {
         if (hash.size() <= 0 || newRef.size() <= 0) {
